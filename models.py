6ea74b07270b757fd985fa09a4abf8748565858a
Ghyang Lee <168070244+Ghleii@users.noreply.github.com>
2024-09-04 10:46:06 +0900

    error_secondchat

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
